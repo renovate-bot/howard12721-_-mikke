8231e71a289af727f4753205637807c3f27c6de7
howard127 <internal@xhw.jp>
2026-04-22 00:38:04 +0900

    feat: grant privileges to user 'mikke' for multiple services in SQL script

--- backend/docker/debug/mariadb/init/01-create-databases.sql
+++ backend/docker/debug/mariadb/init/01-create-databases.sql
@@ -6,3 +6,12 @@ CREATE DATABASE IF NOT EXISTS guess_service;
 CREATE DATABASE IF NOT EXISTS feed_service;
 CREATE DATABASE IF NOT EXISTS notification_service;
 
+GRANT ALL PRIVILEGES ON identity_service.* TO 'mikke'@'%';
+GRANT ALL PRIVILEGES ON friendship_service.* TO 'mikke'@'%';
+GRANT ALL PRIVILEGES ON post_service.* TO 'mikke'@'%';
+GRANT ALL PRIVILEGES ON media_service.* TO 'mikke'@'%';
+GRANT ALL PRIVILEGES ON guess_service.* TO 'mikke'@'%';
+GRANT ALL PRIVILEGES ON feed_service.* TO 'mikke'@'%';
+GRANT ALL PRIVILEGES ON notification_service.* TO 'mikke'@'%';
+
+FLUSH PRIVILEGES;
