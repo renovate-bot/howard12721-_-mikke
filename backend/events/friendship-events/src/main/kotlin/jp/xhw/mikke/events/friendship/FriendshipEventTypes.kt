2350cdc1c4eaa9bd38b06110761c8db300ef6878
howard127 <internal@xhw.jp>
2026-05-15 13:46:49 +0900

    イベント設計をした

--- backend/events/friendship-events/src/main/kotlin/jp/xhw/mikke/events/friendship/FriendshipEventTypes.kt
+++ backend/events/friendship-events/src/main/kotlin/jp/xhw/mikke/events/friendship/FriendshipEventTypes.kt
@@ -3,6 +3,9 @@ package jp.xhw.mikke.events.friendship
 object FriendshipEventTypes {
     const val REQUESTED = "friendship.requested"
     const val ACCEPTED = "friendship.accepted"
+    const val REJECTED = "friendship.rejected"
+    const val CANCELED = "friendship.canceled"
     const val REMOVED = "friendship.removed"
     const val BLOCKED = "friendship.blocked"
+    const val UNBLOCKED = "friendship.unblocked"
 }
